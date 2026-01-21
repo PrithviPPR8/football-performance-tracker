883a170d74eb022c89a0b1b227623811a6e16de9
Prithviraj Patil <prithvirajpatil64@gmail.com>
2026-01-21 16:47:22 +0530

    Padding added to Calendar from bottom

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Calendar from "@/components/calendar/Calendar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center pt-10">
+    <main className="min-h-screen flex justify-center py-6">
       <div className="bg-white rounded-xl shadow-md p-6 w-full max-w-3xl">
         <Calendar />
       </div>
